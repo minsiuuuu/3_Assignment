48bafe916e716b1eec580738616aa1868b229db8
minsiuuuu <kimms010107@gmail.com>
2024-12-30 15:17:17 +0900

    도전 과제

--- 3_Assignment/3_Assignment.cpp
+++ 3_Assignment/3_Assignment.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,18 +11,44 @@ private:
     T* data;
     int currentSize;
     int currentCapacity;
-    void resize();
+
+    void resize(int newCapacity)
+    {
+        if (newCapacity <= currentCapacity) return;
+
+        T* newData = new T[newCapacity];
+
+        for (int i = 0; i < currentSize; i++)
+        {
+            newData[i] = data[i];
+        }
+
+        delete[] data;
+
+        data = newData;
+        currentCapacity = newCapacity;
+
+    }
 
 public:
     SimpleVector(int capacity = 10) : data(new T[capacity]), currentCapacity(capacity), currentSize(0) {}
+    SimpleVector(const SimpleVector& other) 
+        : data(new T[other.currentCapacity]), currentCapacity(other.currentCapacity), currentSize(other.currentSize) 
+    {
+        for (int i = 0; i < currentSize; i++)
+        {
+            data[i] = other.data[i];
+        }
+    }
     ~SimpleVector() { delete[] data; }
 
     void push_back(const T& value) 
     {
-        if (currentCapacity > currentSize)
+        if (currentCapacity <= currentSize)
         {
-            data[currentSize++] = value;
+            resize(currentSize + 5);
         }
+        data[currentSize++] = value;
     }
     void pop_back() 
     {
@@ -37,7 +64,16 @@ public:
     {
         for (int i = 0; i < currentSize; i++)
         {
-            cout << data[i] << endl;
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void sortData()
+    {
+        if (currentSize > 1)
+        {
+            sort(data, data + currentSize);
         }
     }
 };
@@ -48,7 +84,14 @@ int main()
     sv.push_back(10);
     sv.push_back(5);
     sv.push_back(15);
+    sv.sortData();
     sv.pop_back();
+    sv.printArray();
+
+    SimpleVector<int> svCopy(sv);
+    svCopy.push_back(20);
+    svCopy.printArray();
+
     sv.printArray();
 
     return 0;
